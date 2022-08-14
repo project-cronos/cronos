3344316108c39edf3a711ef8afe00c0e6dc1156b
Yiting Shen <shenyiting2018@gmail.com>
2022-08-14 14:58:50 -0700

    add model comment

--- cronos/models/job.py
+++ cronos/models/job.py
@@ -1,12 +1,21 @@
 from django.db import models
 
 
+# This model stores the basic information of jobs
 class Job(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    name = models.CharField(max_length=64, unique=True)
-    cron_expression = models.CharField(max_length=32)
-    is_inactive = models.BooleanField(default=False)
+    id = models.BigAutoField(primary_key=True)  # the id of the job
+    name = models.CharField(max_length=64, unique=True)  # the name of the job
+    cron_expression = models.CharField(max_length=32)  # the cron expression of the job
+    is_inactive = models.BooleanField(
+        default=False
+    )  # the sign shows whether the job is inactive
     # job_group_id = models.BigIntegerField(null=True)
-    service_id = models.CharField(max_length=64, null=True)
-    created_time = models.DateTimeField(auto_now_add=True)
-    updated_time = models.DateTimeField(auto_now=True)
+    service_id = models.CharField(
+        max_length=64, null=True
+    )  # the identifier of the service that should run the job
+    created_time = models.DateTimeField(
+        auto_now_add=True
+    )  # the time of the job was created
+    updated_time = models.DateTimeField(
+        auto_now=True
+    )  # the latest time of the job was updated
