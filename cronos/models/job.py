8b3fc70eb11f2b8077b151abc7204111e09e910e
Yiting Shen <shenyiting2018@gmail.com>
2022-08-14 14:47:46 -0700

    create job model

--- cronos/models/job.py
+++ cronos/models/job.py
@@ -1,3 +1,12 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class Job(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=64, unique=True)
+    cron_expression = models.CharField(max_length=32)
+    is_inactive = models.BooleanField(default=False)
+    # job_group_id = models.BigIntegerField(null=True)
+    service_id = models.CharField(max_length=64, null=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
